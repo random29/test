d076dd1c3150942c4282325513e7a3572c605ceb
warrk <test@test.test>
2018-12-02 16:34:25 +0300

    test

--- src/main/java/ru/itpark/sevice/Chacback.java
+++ src/main/java/ru/itpark/sevice/Chacback.java
@@ -1,7 +1,7 @@
 package ru.itpark.sevice;
 
 public class Chacback {
-    public int cachback(int amount)
-    {
-        return amount*5/100;
-    }}
+    public int cachback(int amount) {
+        return amount * 5 / 100;
+    }
+}
